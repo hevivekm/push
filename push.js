19f56a335722174b8bdd77811f23a844c54e4158
Vivek Molkar <13690485+hevivekm@users.noreply.github.com>
2020-09-02 15:27:15 +0530

    fix 3

--- push.js
+++ push.js
@@ -7,6 +7,14 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:vivek.m@hubengage.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {
+  endpoint:
+    'https://fcm.googleapis.com/fcm/send/cdGvBkNLbBA:APA91bGXGIx9wO3TPrmeTQOPGY4gUic_IDi8jHdrnt6-F3z0uA8iXjDvIBkFhI6sQ4iJiHtImyEOiqWuYzYw6ITl9Q9A30Oe9ePE2csWWKiEuDDDT3fXhuRGi9P-6vnOSs8vUOWJM7Ug',
+  expirationTime: null,
+  keys: {
+    p256dh: 'BOu0B7egOycibbOsV_hFLs9TJ-an1KRl44UU0Is4VE6NswV_hzJwjj6NxcIwugOTpZ88PoYq7I5LfgE13nn6cYs',
+    auth: 'Ev8pYhikPl8f99sKiJJxiA',
+  },
+};
 
 push.sendNotification(sub, 'test message');
